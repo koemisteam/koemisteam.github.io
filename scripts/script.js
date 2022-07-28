31edc90611a91787e319e31e3207db9bc13867e5
Krisious <fransdwikrisdito20@gmail.com>
2022-07-29 02:12:08 +0700

    Adding Char Img Flip 29-07-2022

--- scripts/script.js
+++ scripts/script.js
@@ -4,33 +4,6 @@ window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-// SPLIDE JS
-// Landing page characters
-var lpsplideCharacter = new Splide("#lpsplideCharacter", {
-  perPage: 3,
-  rewind: true,
-  // width: "19.5rem",
-  autoWidth: true,
-  // heigth: "30rem",
-  autoHeight: true,
-  gap: "6.2rem",
-  padding: "2.25rem",
-});
 
-lpsplideCharacter.mount();
-
-// Landing page stories
-var splideStories = new Splide("#splideStory", {
-  perPage: 3,
-  rewind: true,
-  // width: "19.5rem",
-  autoWidth: true,
-  // heigth: "30rem",
-  autoHeight: true,
-  gap: "5.7rem",
-  padding: "2rem",
-});
-
-splideStories.mount();
 
 
